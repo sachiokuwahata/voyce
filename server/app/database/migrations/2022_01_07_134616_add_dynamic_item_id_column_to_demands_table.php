d9bd2159b8a0da90cd270c69771455c7d3af9299
sachiokuwahata <tsachio.kuwahata@gmail.com>
2022-01-07 23:16:18 +0900

    demandsにカラム追加

---
+++ server/app/database/migrations/2022_01_07_134616_add_dynamic_item_id_column_to_demands_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDynamicItemIdColumnToDemandsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('demands', function (Blueprint $table) {
+            $table->integer('dynamic_item_id')->after('values');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('demands', function (Blueprint $table) {
+            $table->dropColumn('dynamic_item_id');
+        });
+    }
+}
