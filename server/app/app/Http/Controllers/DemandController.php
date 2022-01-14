842ea5444574397dea4868b5719e8e1bce611066
sachiokuwahata <tsachio.kuwahata@gmail.com>
2022-01-14 21:48:35 +0900

    DBファザード処理

--- server/app/app/Http/Controllers/DemandController.php
+++ server/app/app/Http/Controllers/DemandController.php
@@ -69,32 +69,32 @@ class DemandController extends Controller
 
     public function itemEntryDone(Request $request)
     {
-        $data_type_id = $request->input('data_type_id');
 
-        $dynamicitem = DynamicItem::create([
-            'label' => $request->input('label'),
-            'required' => $request->boolean('required'),
-            'data_type_id' => $data_type_id,
-        ]);
+        DB::transaction(function () use ($request) {
 
+            $data_type_id = $request->input('data_type_id');
 
-        if($data_type_id == 4){
-
-            // 確認：選択肢の登録処理方法
-            $new_dynamicitem_id = $dynamicitem->id;
-            $choices_str = $request->input('choices');
-
-            $choices = explode(",",$choices_str);
-            foreach ($choices as $choice){
-                DynamicItemChoice::create([
-                    'dynamic_item_id' => $new_dynamicitem_id,
-                    'choices' => $choice,
-                ]);
-            }
-
-
-        }
-
+            $dynamicitem = DynamicItem::create([
+                'label' => $request->input('label'),
+                'required' => $request->boolean('required'),
+                'data_type_id' => $data_type_id,
+            ]);
+    
+            if($data_type_id == 4){
+    
+                // 確認：選択肢の登録処理方法
+                $new_dynamicitem_id = $dynamicitem->id;
+                $choices_str = $request->input('choices');
+    
+                $choices = explode(",",$choices_str);
+                foreach ($choices as $choice){
+                    DynamicItemChoice::create([
+                        'dynamic_item_id' => $new_dynamicitem_id,
+                        'choices' => $choice,
+                    ]);
+                }
+            }    
+        });
 
         return redirect()->route('demand.showItemAll');
     }
