446a8cf966f0657107b311745ed40aabb7acd16e
sachiokuwahata <tsachio.kuwahata@gmail.com>
2022-01-26 18:37:18 +0900

    client周りのテーブル追加

---
+++ server/app/database/migrations/2022_01_26_092725_create_client_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('client_details', function (Blueprint $table) {
+            $table->id();
+            $table->integer('client_id');
+            $table->integer('company_id');
+            $table->integer('dynamic_item_id');
+            $table->string('values', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('client_details');
+    }
+}
---
+++ server/app/database/migrations/2022_01_26_092748_create_client_dynamic_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientDynamicItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company_dynamic_items', function (Blueprint $table) {
+            $table->id();
+            $table->integer('company_id');
+            $table->integer('dynamic_item_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('company_dynamic_items');
+    }
+}
